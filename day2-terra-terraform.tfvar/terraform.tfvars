afed63b7f33ffa2268cabd27403ea3a49125ef3b
fardeen <fardeenisa@gmail.com>
2025-02-15 19:03:35 +0530

    day2-terraform

---
+++ day2-terra-terraform.tfvar/terraform.tfvars
@@ -0,0 +1,5 @@
+AMI = "ami-0ddfba243cbee3768"
+INSTANCE_TYPE = "t2.micro"
+KEYPAIR = "Ironman123"
+NAME_OF_INSTANCE = "Terraform_instance"
+SUBNET = "subnet-04b493c59ce7a5f24"
---
+++ day2-terra-terraform.tfvar/variable.tf
@@ -0,0 +1,25 @@
+variable "AMI" {
+    description = "contains ami id"
+    type = string
+    default = ""
+}
+variable "INSTANCE_TYPE" {
+  description = "contains_instance_type"
+    type = string
+    default = ""
+}
+variable "KEYPAIR" {
+  description = "contains_keypair"
+    type = string
+    default = ""
+}
+variable "NAME_OF_INSTANCE" {
+  description = "conatins_instance_name"
+    type = string
+    default = ""
+}
+variable "SUBNET" {
+  description = "contains_subnet_id"
+    type = string
+    default = ""
+}
