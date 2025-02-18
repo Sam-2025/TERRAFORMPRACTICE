a37c221db58e61a038ad2480f5fed7eae6e5cdd9
fardeen <fardeenisa@gmail.com>
2025-02-18 14:30:11 +0530

    finally conflict solved

--- Day5-CustNetwork/privatenetwork-samevpc.tf
+++ Day5-CustNetwork/privatenetwork-samevpc.tf
@@ -36,7 +36,7 @@ resource "aws_route_table" "private" {
 }
 
 resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.cust.id
+  subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.private.id
 }
 
