f160e40f9834a50528b01ed6f912f4837729f581
Fardeen Attar <fardeenisa@gmail.com>
2025-02-22 18:21:42 +0530

    Update manualresource.tf

--- Day6-All/Day6-import-manualresource/manualresource.tf
+++ Day6-All/Day6-import-manualresource/manualresource.tf
@@ -1,5 +1,5 @@
-resource "aws_instance" "kheencho" {
-  ami = "ami-0ddfba243cbee3768"
+resource "aws_instance" "test" {
+  ami = "ami-554646426445sd4"
   instance_type = "t2.micro"
   key_name = "Ironman123"
   tags = {
