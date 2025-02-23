bcf9fa4ab463d8e97c92c59fd724ab316a3b4536
Fardeen Attar <fardeenisa@gmail.com>
2025-02-23 11:57:24 +0530

    Update rds.tf

--- Day9-All-Modules/rds/rds.tf
+++ Day9-All-Modules/rds/rds.tf
@@ -15,9 +15,9 @@ module "free_tier_rds" {
   iam_database_authentication_enabled = true
   publicly_accessible                 = true
 
-  vpc_security_group_ids = ["sg-sd6s5563d6e6s5d6w66e"]
+  vpc_security_group_ids = ["sg-sd6s5563d655342466e"]
   create_db_subnet_group = true
-  subnet_ids             = ["subnet-123456789101112", "subnet-987654321987654"]
+  subnet_ids             = ["subnet-123456789101112", "subnet-98765432556h54"]
 
 
   backup_retention_period = 7
