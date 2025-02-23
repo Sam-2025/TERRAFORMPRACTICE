3fc6969293e20b4d7e29d2cf00811e7610ee3391
Fardeen Attar <fardeenisa@gmail.com>
2025-02-23 12:11:23 +0530

    Update main.tf

--- Day9-All-Modules/root-module/main.tf
+++ Day9-All-Modules/root-module/main.tf
@@ -5,9 +5,9 @@ provider "aws" {
 module "server_instance" {
   source         = "./modules/ec2-child-module"
   machine_size   = "t2.micro"
-  ssh_key        = "Ironman123"
-  image          = "ami-0d682f26195e9ec0f"
-  subnt          = "subnet-0cda5ad3e644c6d65"
+  ssh_key        = "key"
+  image          = "ami-0d682abcde933ec0f"
+  subnt          = "subnet-0646946wd65"
   tag            = "Module"
 }
 
