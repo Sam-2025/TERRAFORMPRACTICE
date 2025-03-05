a267e2f9e47bc280e70269260ece2aa2feaabaa7
fardeen <fardeenisa@gmail.com>
2025-03-05 14:30:07 +0530

    automated code for jenkins server

--- Jenkins/main.tf
+++ Jenkins/main.tf
@@ -1,11 +1,66 @@
+/*========================================== Provider ==================================================*/
+
 provider "aws" {
 
 }
+/*========================================== Locals =====================================================*/
 locals {
-  keypair  = "Ironman123"
-  typeinst = "t2.medium"
+  typeinst = "t2.micro"
   tag      = "J-E-N-K-I-N-S"
 }
+
+/*====================================== KeyPair & Instance ==============================================*/
+
+resource "aws_key_pair" "pubkey" {
+  key_name = "JenkinServerKey"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+  data "aws_subnet" "az" {
+    filter {
+      name = "tag:Name"
+      values = ["test"]
+    }
+  }
+
+  resource "aws_instance" "jenkins" {
+  ami           = data.aws_ami.fardeen.id
+  subnet_id     = data.aws_subnet.mumb.id
+  key_name      = aws_key_pair.pubkey.key_name
+  instance_type = local.typeinst
+  associate_public_ip_address = true
+  user_data     = file("jenkin.sh")
+  tags = {
+    Name = local.tag
+  }
+  provisioner "remote-exec" {
+   inline = [
+    "echo 'Granting full permissions...'",
+    "sudo chmod -R 777 /var/lib/jenkins/",
+
+    "echo 'Starting Jenkins service...'",
+    "sudo systemctl start jenkins",
+    "sudo systemctl enable jenkins",
+
+    "echo 'Waiting for Jenkins to initialize (Max 5 minutes)...'",
+    "timeout 300 sh -c 'while [ ! -f /var/lib/jenkins/secrets/initialAdminPassword ]; do sleep 5; done'",
+
+    "echo 'Jenkins Initial Admin Password:'",
+    "sudo cat /var/lib/jenkins/secrets/initialAdminPassword"
+     ]
+  }
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    private_key = file("~/.ssh/id_rsa")
+    host = self.public_ip
+  }
+  depends_on = [ aws_key_pair.pubkey ]
+}
+
+
+/*========================================== Data-Source ===================================================*/
+
 data "aws_ami" "fardeen" {
   most_recent = true
   owners      = ["amazon"]
@@ -27,13 +82,17 @@ data "aws_ami" "fardeen" {
 
   }
 }
+
+
 data "aws_subnet" "mumb" {
   filter {
     name   = "tag:Name"
     values = ["test"]
   }
-
 }
+
+/*============================================= Output Block ===================================================*/
+
 output "aws_ami" {
   value = data.aws_ami.fardeen.image_id
 }
@@ -46,15 +105,6 @@ output "awssubnttag" {
 output "awsinst" {
   value = aws_instance.jenkins.id
 }
-
-resource "aws_instance" "jenkins" {
-  ami           = data.aws_ami.fardeen.id
-  subnet_id     = data.aws_subnet.mumb.id
-  key_name      = local.keypair
-  instance_type = local.typeinst
-  associate_public_ip_address = true
-  user_data     = file("jenkin.sh")
-  tags = {
-    Name = local.tag
-  }
+output "publi_ip_of_the_instance" {
+  value = aws_instance.jenkins.public_ip
 }
