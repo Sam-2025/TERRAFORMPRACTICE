4c43ea14728e4420244074a2cb4ed41c2872e873
FardeenAttar <fardeenisa@gmail.com>
2025-03-05 17:40:46 +0530

    Update main.tf

--- sample-ec2/main.tf
+++ sample-ec2/main.tf
@@ -6,12 +6,7 @@ resource "aws_instance" "sample" {
   subnet_id = "subnet-0cda5ad3e644c6d65"
   key_name = "Ironman123"
   instance_type = "t2.micro"
-  iam_instance_profile = aws_iam_instance_profile.profile.name
   tags = {
     Name = "server-created-by-jenkins"
   }
-}
-resource "aws_iam_instance_profile" "profile" {
-  name = "myec2-adminrole"
-  role = "EC2_ADMIN"
 }
