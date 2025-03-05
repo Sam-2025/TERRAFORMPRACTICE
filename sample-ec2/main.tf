de02ee54c2fd54a14b6146ab9e6bbbb8f2b3bef2
fardeen <fardeenisa@gmail.com>
2025-03-05 16:54:05 +0530

    sample server

--- sample-ec2/main.tf
+++ sample-ec2/main.tf
@@ -3,10 +3,15 @@ provider "aws" {
 }
 resource "aws_instance" "sample" {
   ami = "ami-0d682f26195e9ec0f"
-  subnet_id = "subnet-07282fa9802d2992e"
-  key_name = "samkey"
+  subnet_id = "subnet-0cda5ad3e644c6d65"
+  key_name = "Ironman123"
   instance_type = "t2.micro"
+  iam_instance_profile = aws_iam_instance_profile.profile.name
   tags = {
-    Name = "pipeline-server"
+    Name = "server-created-by-jenkins"
   }
+}
+resource "aws_iam_instance_profile" "profile" {
+  name = "myec2-adminrole"
+  role = "EC2_ADMIN"
 }
