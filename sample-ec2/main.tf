d7b314263069c6585c1f8260260764d0f28899e3
Sam-2025 <smunaff64@gmail.com>
2025-03-06 18:11:05 +0530

    Update main.tf

--- sample-ec2/main.tf
+++ sample-ec2/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 resource "aws_instance" "sample" {
   ami = "ami-0d682f26195e9ec0f"
-  subnet_id = "subnet-07282fa9802d2992e"
+  subnet_id = "subnet-0cb2f698ca5329836"
   key_name = "samkey"
   instance_type = "t3.micro"
   tags = {
